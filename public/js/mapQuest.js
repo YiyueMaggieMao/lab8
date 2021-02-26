c608846303fbecee64ec99487bdf5492c2fa4434
YiyueMaggieMao <myykitty711@gmail.com>
2021-02-26 16:28:04 +0800

    Lab 8 complete

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'mEBLBkhbpIgMPkC4nUqWS9S49s21z8A1';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+	center: [32.88, -117.238],
+	layers: L.mapquest.tileLayer('map'),
+	zoom: 12,
+	zoomControl: false
+	});
+
+	// Add a marker to the map
+	L.marker([32.88, -117.238]).addTo(map);
 }
